22213d9ef1d86459f10fbf2d788b17897a5d3e69
protolambda <proto@protolambda.com>
2023-06-29 13:02:44 -0600

    csv time column

--- csv.go
+++ csv.go
@@ -7,16 +7,16 @@ import (
 	"strconv"
 )
 
-func WriteCSVMetrics[E any](ctx context.Context, aggMetric AggregateMetric[E], w io.Writer, elems <-chan E) error {
+func WriteCSVMetrics[E any](ctx context.Context, timeFn func(elem E) int64, aggMetric AggregateMetric[E], w io.Writer, elems <-chan E) error {
 	dest := make([]float64, len(aggMetric.Names), len(aggMetric.Names))
-	buf := make([]byte, len(dest)*10)
+	buf := make([]byte, (1+len(dest))*10)
 	line := 0
 	for {
 		// clean the array
 		for i := range dest {
 			dest[i] = 0
 		}
-		// clean the write buffer
+		// reset the write buffer
 		buf = buf[:0]
 		// get the next element
 		select {
@@ -26,20 +26,22 @@ func WriteCSVMetrics[E any](ctx context.Context, aggMetric AggregateMetric[E], w
 			if !ok {
 				return nil
 			}
+			t := timeFn(elem)
 			// collect metrics values
 			if err := aggMetric.Fn(elem, dest); err != nil {
-				return fmt.Errorf("failed to collect line %d metric: %w", line, err)
+				return fmt.Errorf("failed to collect line %d (t=%d) metric: %w", line, t, err)
 			}
+			// write timestamp
+			buf = strconv.AppendInt(buf, t, 10)
 			// format metrics values and write to buffer
 			for _, v := range dest {
-				buf = strconv.AppendFloat(buf, v, 'f', 8, 64)
 				buf = append(buf, ',')
+				buf = strconv.AppendFloat(buf, v, 'f', 8, 64)
 			}
-			// overwrite last comma
-			buf[len(buf)-1] = '\n'
+			buf = append(buf, '\n')
 			// write to output
 			if _, err := w.Write(buf); err != nil {
-				return fmt.Errorf("failed to write line %d: %w", line, err)
+				return fmt.Errorf("failed to write line %d (t=%d): %w", line, t, err)
 			}
 		}
 		line += 1
